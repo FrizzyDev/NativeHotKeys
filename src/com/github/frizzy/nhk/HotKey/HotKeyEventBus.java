c17fbec81c9ee62e38195f4fcb4e5d1f0d21b9eb
frizz <frizzydev@proton.me>
2024-10-22 20:29:31 -0500

    Adjusted input processing logic to prevent certain hotkey combinations. Added setPause(boolean) to pause input processing. Added Hotkey.buildStringRepresentation to convert the int[] expression array to a String representation. Added the ability to bind a Hotkey to a specific HotkeyListener.

--- src/com/github/frizzy/nhk/HotKey/HotKeyEventBus.java
+++ src/com/github/frizzy/nhk/HotKey/HotKeyEventBus.java
@@ -3,8 +3,7 @@ package com.github.frizzy.nhk.HotKey;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 
-import java.util.Collections;
-import java.util.Set;
+import java.util.*;
 import java.util.logging.Logger;
 
 /**
@@ -32,7 +31,7 @@ public class HotKeyEventBus implements NativeKeyListener {
     /**
      * Logger for the HotKeyEventBus.
      */
-    protected static Logger log = Logger.getLogger ( HotKeyEventBus.class.getName () );
+    protected static Logger log = Logger.getLogger ( HotKeyEventBus.class.getName ( ) );
 
     /**
      * <p>
@@ -42,7 +41,14 @@ public class HotKeyEventBus implements NativeKeyListener {
      * A Set is used to ensure listeners added are unique.
      * </p>
      */
-    private Set< HotKeyListener > listeners;
+    private final Set< HotKeyListener > listenerSet;
+
+    /**
+     * <p>
+     * A map used to dispatch certain Hotkeys to specific HotKeyListeners.
+     * </p>
+     */
+    private final Map< Hotkey, HotKeyListener > listenerMap;
 
     /**
      * <p>
@@ -85,13 +91,21 @@ public class HotKeyEventBus implements NativeKeyListener {
     /**
      * If true, the event bus will output key events to the console.
      */
-    private boolean debug = false;
+    private boolean debug;
+
+    /**
+     * <p>
+     * If paused, the event bus will stop tracking input events.
+     * </p>
+     */
+    private boolean paused = false;
 
     /**
      * Creates a HotKeyEventBus with no HotKeys.
      */
+    @SuppressWarnings ( "unused" )
     public HotKeyEventBus ( ) {
-        this ( Collections.emptySet () );
+        this ( Collections.emptySet ( ) );
     }
 
     /**
@@ -103,7 +117,7 @@ public class HotKeyEventBus implements NativeKeyListener {
      * </p>
      */
     public HotKeyEventBus ( boolean debug ) {
-        this ( Collections.emptySet () , debug );
+        this ( Collections.emptySet ( ) , debug );
     }
 
     /**
@@ -118,7 +132,8 @@ public class HotKeyEventBus implements NativeKeyListener {
      */
     private HotKeyEventBus ( final Set< Hotkey > hotkeys , boolean debug ) {
         this.hotkeys = hotkeys;
-        listeners = Collections.emptySet ();
+        listenerSet = new HashSet<> ( );
+        listenerMap = new HashMap<> ( );
         this.debug = debug;
     }
 
@@ -133,20 +148,30 @@ public class HotKeyEventBus implements NativeKeyListener {
      */
     @Override
     public void nativeKeyPressed ( NativeKeyEvent nativeEvent ) {
-        int code = nativeEvent.getKeyCode ( );
-        debug (  "Received event key code on keyPressed: " + code + ". Code text: " + NativeKeyEvent.getKeyText ( code ) + "."  );
+        if ( !paused ) {
+            int code = nativeEvent.getKeyCode ( );
+            debug ( "Received event key code on keyPressed: " + code + ". Code text: " + NativeKeyEvent.getKeyText ( code ) + "." );
 
-        if ( isModifierCode ( code ) ) {
-            debug ( "Received event key code is a modifier code." );
+            if ( isModifierCode ( code ) ) {
+                debug ( "Received event key code is a modifier code." );
 
-            if ( expression[ MODIFIER_1 ] == INVALID_CODE && expression[ MODIFIER_2 ] == INVALID_CODE ) {
-                debug ( "Setting MODIFIER_1 to code: " + code + ". Code text: " + NativeKeyEvent.getKeyText ( code ) + "." );
+                if ( expression[ MODIFIER_1 ] == INVALID_CODE && expression[ MODIFIER_2 ] == INVALID_CODE ) {
+                    debug ( "Setting MODIFIER_1 to code: " + code + ". Code text: " + NativeKeyEvent.getKeyText ( code ) + "." );
 
-                expression[ MODIFIER_1 ] = code;
-            } else if ( expression[ MODIFIER_1 ] != -1 && expression[ MODIFIER_2 ] == -1 ) {
-                debug ( "Setting MODIFIER_2 to code: " + code + ". Code text: " + NativeKeyEvent.getKeyText ( code ) + "." );
+                    expression[ MODIFIER_1 ] = code;
+                } else if ( expression[ MODIFIER_1 ] != -1 && expression[ MODIFIER_2 ] == -1 ) {
+                    debug ( "Setting MODIFIER_2 to code: " + code + ". Code text: " + NativeKeyEvent.getKeyText ( code ) + "." );
 
-                expression[ MODIFIER_2 ] = code;
+                    /*
+                     * Check to make sure the modifier code at the start of the
+                     * expression is not the same as the one we want to assign as
+                     * the second modifier code. This should prevent Hotkeys such
+                     * as Control + Control + C.
+                     */
+                    if ( code != expression[ MODIFIER_1 ] ) {
+                        expression[ MODIFIER_2 ] = code;
+                    }
+                }
             }
         }
     }
@@ -163,45 +188,78 @@ public class HotKeyEventBus implements NativeKeyListener {
      */
     @Override
     public void nativeKeyReleased ( NativeKeyEvent nativeEvent ) {
-        int code = nativeEvent.getKeyCode ( );
-        debug ( "Received event key code on keyReleased: " + code + ". Key text: " + NativeKeyEvent.getKeyText ( code ) + "." );
+        if ( !paused ) {
+            int code = nativeEvent.getKeyCode ( );
+            debug ( "Received event key code on keyReleased: " + code + ". Key text: " + NativeKeyEvent.getKeyText ( code ) + "." );
 
-        if ( isModifierCode ( code ) ) {
-            debug ( "Received event key code is a modifier code." );
+            if ( isModifierCode ( code ) ) {
+                debug ( "Received event key code is a modifier code." );
 
-            if ( expression[ MODIFIER_1 ] == code ) {
-                debug ( "Expression code at index of MODIFIER_1 is keyReleased event code. MODIFIER_1 is set to INVALID_CODE." );
+                if ( expression[ MODIFIER_1 ] == code ) {
+                    debug ( "Expression code at index of MODIFIER_1 is keyReleased event code. MODIFIER_1 is set to INVALID_CODE." );
 
-                expression[ MODIFIER_1 ] = INVALID_CODE;
-            } else if ( expression[ MODIFIER_2 ] == code ) {
-                debug ( "Expression code at index of MODIFIER_2 is keyReleased event code. MODIFIER_2 is set to INVALID_CODE." );
+                    expression[ MODIFIER_1 ] = INVALID_CODE;
+                } else if ( expression[ MODIFIER_2 ] == code ) {
+                    debug ( "Expression code at index of MODIFIER_2 is keyReleased event code. MODIFIER_2 is set to INVALID_CODE." );
 
-                expression[ MODIFIER_2 ] = INVALID_CODE;
-            }
-        } else {
-            debug ( "Received event key code on keyReleased is NOT a modifier code." );
+                    expression[ MODIFIER_2 ] = INVALID_CODE;
+                }
+            } else {
+                debug ( "Received event key code on keyReleased is NOT a modifier code." );
 
-            if ( checkCodeValidity ( code ) ) {
-                debug ( "Received event key code on keyReleased is a valid binding code. Setting expression code at index KEYCODE to code: " + code + ". Code text: " + NativeKeyEvent.getKeyText ( code ) + "." );
+                if ( checkCodeValidity ( code ) ) {
+                    debug ( "Received event key code on keyReleased is a valid binding code. Setting expression code at index KEYCODE to code: " + code + ". Code text: " + NativeKeyEvent.getKeyText (
+                            code ) + "." );
 
-                expression[ KEYCODE ] = code;
-                checkExpression ( );
+                    expression[ KEYCODE ] = code;
+                    checkExpression ( );
+                }
             }
         }
     }
 
     /**
-     * Sets the hotkeys list to the provided list.
+     * <p>
+     * Sets the HotKeyEventBus to stop processing input events.
+     * </p>
      */
-    public void setHotkeys ( final Set< Hotkey > hotkeys ) {
-        this.hotkeys = hotkeys;
-        log.info ( "Provided Hotkeys Set was set to the HotKeyEventBus.");
+    @SuppressWarnings ( "unused" )
+    public void setPaused ( boolean paused ) {
+        this.paused = paused;
+
+        if ( paused ) {
+            log.info ( "HotkeyEventBus was paused. Input events will not be processed." );
+        } else {
+            log.info ( "HotkeyEventBus was unpaused. Input events will now be processed." );
+        }
     }
 
     /**
-     * Adds the provided Hotkey to the HotKeyEventBus.
+     * <p>
+     * Sets the hotkeys list to the provided list.
+     * </p>
+     * <p>
+     * Hotkeys registered this will way will notify all HotkeyListeners that
+     * are attached to the HotkeyEventBus.
+     * </p>
      */
-    public void addHotkey ( final Hotkey hotkey ) {
+    @SuppressWarnings ( "unused" )
+    public void registerHotkeys ( final Set< Hotkey > hotkeys ) {
+        this.hotkeys = hotkeys;
+        log.info ( "Provided Hotkeys Set was set to the HotKeyEventBus." );
+    }
+
+    /**
+     * <p>
+     * Adds the provided Hotkey to the HotkeyEventBus.
+     * </p>
+     * <p>
+     * Hotkeys registered this will way will notify all HotkeyListeners that
+     * are attached to the HotkeyEventBus.
+     * </p>
+     */
+    @SuppressWarnings ( "unused" )
+    public void registerHotkey ( final Hotkey hotkey ) {
         boolean changed = hotkeys.add ( hotkey );
 
         if ( changed ) {
@@ -212,8 +270,20 @@ public class HotKeyEventBus implements NativeKeyListener {
     }
 
     /**
+     * <p>
+     * Registers the provided Hotkey to the provided HotKeyListener, so whenever the Hotkey
+     * is pressed, ONLY the provided listener will be called.
+     * </p>
+     */
+    @SuppressWarnings ( "unused" )
+    public void registerHotkeyWithListener ( final Hotkey hotkey , HotKeyListener listener ) {
+        listenerMap.put ( hotkey , listener );
+    }
+
+    /**
      * Removes the provided Hotkey from the HotKeyEventBus.
      */
+    @SuppressWarnings ( "unused" )
     public void removeHotKey ( final Hotkey hotkey ) {
         boolean contained = hotkeys.remove ( hotkey );
 
@@ -225,10 +295,20 @@ public class HotKeyEventBus implements NativeKeyListener {
     }
 
     /**
+     * Removes the provided Hotkey and its corresponding HotkeyListener from
+     * the HotkeyEventBus.
+     */
+    @SuppressWarnings ( "unused" )
+    public void removeHotkeyWithListener ( final Hotkey hotkey ) {
+        listenerMap.remove ( hotkey );
+    }
+
+    /**
      * Adds the provided HotKeyListener to the HotKeyEventBus.
      */
+    @SuppressWarnings ( "unused" )
     public void addHotKeyListener ( HotKeyListener listener ) {
-        boolean contained = listeners.add ( listener );
+        boolean contained = listenerSet.add ( listener );
 
         if ( contained ) {
             log.info ( "Provided HotKeyListener was registered with the HotKeyEventBus." );
@@ -237,8 +317,14 @@ public class HotKeyEventBus implements NativeKeyListener {
         }
     }
 
+    /**
+     * <p>
+     * Removes the provided HotkeyListener from the HotkeyEventBus.
+     * </p>
+     */
+    @SuppressWarnings ( "unused" )
     public void removeHotKeyListener ( final HotKeyListener listener ) {
-        boolean contained = listeners.remove ( listener );
+        boolean contained = listenerSet.remove ( listener );
 
         if ( contained ) {
             log.info ( "Provided HotKeyListener was registered with the HotKeyEventBus. The listener has been removed." );
@@ -248,16 +334,29 @@ public class HotKeyEventBus implements NativeKeyListener {
     }
 
     /**
+     * <p>
      * Checks the key expression recorded by the NativeKeyListener.
+     * </p>
+     * <p>
+     * If a matching Hotkey is found, the HotkeyListeners added to the listenerSet
+     * will be notified, OR, if the Hotkey is contained in the listenerMap, only that specific
+     * HotkeyListener will be notified.
+     * </p>
      */
     private void checkExpression ( ) {
-        debug ( "Checking completed key event expression for a valid Hotkey." );
+        debug ( "Checking for matching Hotkey for expression: " + Hotkey.buildStringRepresentation ( expression ) + "." );
 
         for ( Hotkey key : hotkeys ) {
             if ( key.matchesKeyExpression ( expression ) ) {
                 debug ( "Matching Hotkey was determined. Notifying attached listeners of the event." );
 
                 notifyListeners ( new HotKeyEvent ( key ) );
+            }
+        }
+
+        for ( Hotkey key : listenerMap.keySet ( ) ) {
+            if ( key.matchesKeyExpression ( expression ) ) {
+                listenerMap.get ( key ).hotkeyPressed ( new HotKeyEvent ( key ) );
             }
         }
 
@@ -268,7 +367,7 @@ public class HotKeyEventBus implements NativeKeyListener {
      * Notifies any attached HotKeyListeners.
      */
     private void notifyListeners ( HotKeyEvent event ) {
-        for ( HotKeyListener hkls : listeners ) {
+        for ( HotKeyListener hkls : listenerSet ) {
             hkls.hotkeyPressed ( event );
         }
     }
--- src/com/github/frizzy/nhk/HotKey/Hotkey.java
+++ src/com/github/frizzy/nhk/HotKey/Hotkey.java
@@ -13,34 +13,34 @@ import java.util.Objects;
  * @version 0.1
  * @since 0.1
  */
-public class Hotkey implements Comparable<Hotkey> {
+public class Hotkey implements Comparable< Hotkey > {
 
     /**
      * The ID of the process that should be started when
      * this HotKey is dispatched.
      */
-    private String commandID;
+    private final String commandID;
 
     /**
      * The text representation of the hotkey. For example,
      * this could appear as "Control + Shift + Space"
      */
-    private String expressionText;
+    private final String expressionText;
 
     /**
      * The first modifier for this Hotkey.
      */
-    private int mod1 = -1;
+    private final int mod1;
 
     /**
      * The second modifier for this Hotkey.
      */
-    private int mod2 = -1;
+    private final int mod2;
 
     /**
      * The keycode for this hotkey.
      */
-    private int key = -1;
+    private final int key;
 
     /**
      * Constructs the Hotkey with the provided commandID, first and second key modifiers,
@@ -48,17 +48,17 @@ public class Hotkey implements Comparable<Hotkey> {
      * key codes represented by -1 means that it has not been set.
      *
      * @param commandID The commandID of the process that should be started when this hotkey is dispatched.
-     * @param mod1  The first modifier of the hotkey expression, such as Shift or Control.
-     * @param mod2 The second modifier of the hotkey expression, such as Shift or Control.
-     * @param key The key code of the hotkey expression, such as letters, numbers, or function buttons.
+     * @param mod1      The first modifier of the hotkey expression, such as Shift or Control.
+     * @param mod2      The second modifier of the hotkey expression, such as Shift or Control.
+     * @param key       The key code of the hotkey expression, such as letters, numbers, or function buttons.
      */
-    public Hotkey ( String commandID, int mod1, int mod2, int key ) {
+    public Hotkey ( String commandID , int mod1 , int mod2 , int key ) {
         this.commandID = commandID;
         this.mod1 = mod1;
         this.mod2 = mod2;
         this.key = key;
 
-        StringBuilder builder = new StringBuilder (  );
+        StringBuilder builder = new StringBuilder ( );
 
         if ( mod1 != -1 ) {
             builder.append ( NativeKeyEvent.getKeyText ( mod1 ) ).append ( " + " );
@@ -74,7 +74,7 @@ public class Hotkey implements Comparable<Hotkey> {
             builder.append ( "Unknown key code. Please rebind your hotkey for this command: " ).append ( commandID );
         }
 
-        expressionText = builder.toString ();
+        expressionText = builder.toString ( );
     }
 
     /**
@@ -88,7 +88,7 @@ public class Hotkey implements Comparable<Hotkey> {
      * </p>
      */
     public boolean matchesKeyExpression ( int[] expression ) {
-         return expression[ HotKeyEventBus.MODIFIER_1 ] == mod1 &&
+        return expression[ HotKeyEventBus.MODIFIER_1 ] == mod1 &&
                 expression[ HotKeyEventBus.MODIFIER_2 ] == mod2 &&
                 expression[ HotKeyEventBus.KEYCODE ] == key;
     }
@@ -102,6 +102,7 @@ public class Hotkey implements Comparable<Hotkey> {
      * Hotkey binding.
      * </p>
      */
+    @SuppressWarnings ( "unused" )
     public boolean matchesExpressionText ( String expText ) {
         return expText.equals ( expressionText );
     }
@@ -123,6 +124,7 @@ public class Hotkey implements Comparable<Hotkey> {
      * Returns the first modifier of the HotKey expression, if there is one.
      * If there is not a modifier, the default value returned is -1.
      */
+    @SuppressWarnings ( "unused" )
     public int getFirstModifier ( ) {
         return mod1;
     }
@@ -131,6 +133,7 @@ public class Hotkey implements Comparable<Hotkey> {
      * Returns the second modifier of the HotKey expression, if there is one.
      * If there is not a modifier, the default value returned is -1.
      */
+    @SuppressWarnings ( "unused" )
     public int getSecondModifier ( ) {
         return mod2;
     }
@@ -139,8 +142,30 @@ public class Hotkey implements Comparable<Hotkey> {
      * Returns the key code of the HotKey expression, if there is one.
      * If there is not a modifier, the default value returned is -1.
      */
+    @SuppressWarnings ( "unused" )
     public int getKey ( ) {
         return key;
+    }
+
+    /**
+     * Builds a String representation of the provided Hotkey expression.
+     */
+    public static String buildStringRepresentation ( final int[] expression ) {
+        StringBuilder builder = new StringBuilder ( );
+
+        if ( expression[ HotKeyEventBus.MODIFIER_1 ] != -1 ) {
+            builder.append ( NativeKeyEvent.getKeyText ( expression [ HotKeyEventBus.MODIFIER_1 ] ) ).append ( " + " );
+        }
+
+        if ( expression[ HotKeyEventBus.MODIFIER_2 ] != -1 ) {
+            builder.append ( NativeKeyEvent.getKeyText ( expression [ HotKeyEventBus.MODIFIER_2 ] ) ).append ( " + " );
+        }
+
+        if ( expression [ HotKeyEventBus.KEYCODE ] != -1 ) {
+            builder.append ( NativeKeyEvent.getKeyText ( expression [ HotKeyEventBus.KEYCODE ] ) );
+        }
+
+        return builder.toString ();
     }
 
     /**
@@ -149,7 +174,7 @@ public class Hotkey implements Comparable<Hotkey> {
      */
     @Override
     public int compareTo ( Hotkey o ) {
-        return o.getCommandID ().compareTo ( commandID );
+        return o.getCommandID ( ).compareTo ( commandID );
     }
 
     @Override
